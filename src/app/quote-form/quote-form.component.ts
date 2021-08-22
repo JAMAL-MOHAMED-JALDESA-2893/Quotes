c027426fb695f9cdcd4eee8dbb4d939c4cfca396
JAMAL MOHAMED <jamal.mohamed@student.moringaschool.com>
2021-08-22 15:30:24 +0300

    created a newQuote method to the QuoteFormComponent

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,11 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Quote } from "../quote";
 
 @Component({
   selector: 'app-quote-form',
   templateUrl: './quote-form.component.html',
   styleUrls: ['./quote-form.component.css']
 })
-export class QuoteFormComponent implements OnInit {
+export class QuoteFormComponent  {
+
+
+  newQuote = new Quote(0, '', '', '', new Date(), 0, 0);
+  @Output() addNew = new EventEmitter<Quote>();
+
+  addQuote() {
+this.addNew.emit(this.newQuote);
+  }
 
   constructor() { }
 
@@ -13,3 +22,4 @@ export class QuoteFormComponent implements OnInit {
   }
 
 }
+
