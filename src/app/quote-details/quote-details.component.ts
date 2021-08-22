de97efda031fdc5c6a8b71b8aeccd520845a069c
JAMAL MOHAMED <jamal.mohamed@student.moringaschool.com>
2021-08-22 15:44:49 +0300

    created methods in QouteDetailsComponent class:upvote,downvote and deleteQuote

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Quote } from "../quote";
 
 @Component({
   selector: 'app-quote-details',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteDetailsComponent implements OnInit {
 
+  @Input() quote: Quote;
+  @Output() isRead = new EventEmitter<boolean>();
+
+  deleteQuote(read: boolean) {
+    this.isRead.emit(read);
+  }
+
+  upvote() {
+    this.quote.likes += 1;
+  }
+  downvote() {
+    this.quote.dislike += 1;
+  }
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+
